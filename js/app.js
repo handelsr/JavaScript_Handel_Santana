1aaa623c880275cd6207a2be57b172516f70b5b3
handelsr <31743655+handelsr@users.noreply.github.com>
2018-10-08 02:05:38 -0400

    Edicion 6

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ function dividir(operando1, operando2) {
 
 function limpiar_todo() {
     document.getElementById("display").innerHTML = "0";
-    document.getElementById("oculto").value="0";
+    document.getElementById("oculto").value = "0";
 }
 
 
@@ -74,11 +74,18 @@ function agregar_signo() {
 function calculadora(caracter) {
     //funcion para tomar los caracters dados por el usuario y mostrarlo en pantalla
     var anterior = document.getElementById("display").innerHTML; // controlar lo que se mostrara en pantalla
+    var anteriornum = document.getElementById("display").innerHTML.toString();
+    var anteriornum = anteriornum.toString().replace(".", "");
+    var anteriornum = anteriornum.toString().replace("/", "");
+    var anteriornum = anteriornum.toString().replace("=", "");
+    var anteriornum = anteriornum.toString().replace("+", "");
+    var anteriornum = anteriornum.toString().replace("-", "");
+    var anteriornum = anteriornum.toString().replace("*", "");
     var numeros = "0123456789";
-    var resultado = 0;
+    var resultado = "0";
 
 
-    if (anterior.length < 8) {
+    if (anteriornum.length < 8) {
 
         if (numeros.indexOf(caracter) != -1) { // si el caracter ingresado se encuentra entre los numeros validos, si no  es un signo de operacion
 
@@ -117,55 +124,116 @@ function calculadora(caracter) {
             var operando1;
             var operando2;
 
+            operando2 = document.getElementById("display").innerHTML;
+            operando1 = document.getElementById("oculto").value;
+
             if (signo == "=") {
-                operando2 = document.getElementById("display").innerHTML;
-                operando1 = document.getElementById("oculto").value;
 
 
 
 
                 if (operando1.indexOf("+") != -1) {
-                    operando1 = parseFloat(operando1.split("+"));
-                    operando2 = parseFloat(operando2);
-                    resultado = sumar(operando1, operando2);
-                    document.getElementById("display").innerHTML = resultado;
-                    document.getElementById("oculto").value = operando2 + "+";
+                    if (operando1.indexOf("=") != -1) {
+                        operando1 = parseFloat(operando1.split("+"));
+                        operando2 = parseFloat(operando2);
+                        resultado = sumar(operando1, operando2).toString();
+                        if (resultado.indexOf(".") != -1) {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 9);
+                        }
+                        else {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 8);
+                        }
+                    } else {
+                        operando1 = parseFloat(operando1.split("+"));
+                        operando2 = parseFloat(operando2);
+                        resultado = sumar(operando1, operando2).toString();
+                        document.getElementById("oculto").value = document.getElementById("display").innerHTML + "+=";
+                        if (resultado.indexOf(".") != -1) {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 9);
+                        }
+                        else {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 8);
+                        }
 
-
+                    }
 
 
 
                 } else if (operando1.indexOf("-") != -1) {
+                    if (operando1.indexOf("=") != -1) {
+                        operando1 = parseFloat(operando1.split("-"));
+                        operando2 = parseFloat(operando2);
+                        resultado = restar(operando2, operando1).toString();
+                        if (resultado.indexOf(".") != -1) {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 9);
+                        }
+                        else {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 8);
+                        }
+                    } else {
+                        operando1 = parseFloat(operando1.split("-"));
+                        operando2 = parseFloat(operando2);
+                        resultado = restar(operando1, operando2).toString();
+                        document.getElementById("oculto").value = document.getElementById("display").innerHTML + "-=";
+                        if (resultado.indexOf(".") != -1) {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 9);
+                        }
+                        else {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 8);
+                        }
+                    }
 
-                    operando1 = parseFloat(operando1.split("-"));
-                    operando2 = parseFloat(operando2);
-                    resultado = restar(operando1, operando2);
-                    document.getElementById("display").innerHTML = resultado;
-                    document.getElementById("oculto").value = operando2 + "-";
+                } else if (operando1.indexOf("*") != -1 && document.getElementById("oculto").value != "0") {
+
+                    if (operando1.indexOf("=") != -1) {
+                        operando1 = parseFloat(operando1.split("*"));
+                        operando2 = parseFloat(operando2);
+                        resultado = multiplicar(operando1, operando2).toString();
+                        if (resultado.indexOf(".") != -1) {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 9);
+                        }
+                        else {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 8);
+                        }
+                    } else {
+                        operando1 = parseFloat(operando1.split("*"));
+                        operando2 = parseFloat(operando2);
+                        resultado = multiplicar(operando1, operando2).toString();
+                        document.getElementById("oculto").value = document.getElementById("display").innerHTML + "*=";
+                        if (resultado.indexOf(".") != -1) {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 9);
+                        }
+                        else {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 8);
+                        }
+                    }
 
 
 
-                } else if (operando1.indexOf("*") != -1) {
-                    operando1 = parseFloat(operando1.split("*"));
-                    operando2 = parseFloat(operando2);
-                    resultado = multiplicar(operando1, operando2);
-                    document.getElementById("display").innerHTML = resultado;
-                    document.getElementById("oculto").value = operando2 + "*";
+                } else if (operando1.indexOf("/") != -1 && document.getElementById("oculto").value != "0") {
 
-
-
-
-
-
-                } else if (operando1.indexOf("/") != -1) {
-                    operando1 = parseFloat(operando1.split("/"));
-                    operando2 = parseFloat(operando2);
-                    resultado = dividir(operando1, operando2);
-                    document.getElementById("display").innerHTML = resultado;
-                    document.getElementById("oculto").value = operando2 + "/";
-
-
-
+                    if (operando1.indexOf("=") != -1) {
+                        operando1 = parseFloat(operando1.split("/"));
+                        operando2 = parseFloat(operando2);
+                        resultado = dividir(operando2, operando1).toString();
+                        if (resultado.indexOf(".") != -1) {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 9);
+                        }
+                        else {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 8);
+                        }
+                    } else {
+                        operando1 = parseFloat(operando1.split("/"));
+                        operando2 = parseFloat(operando2);
+                        resultado = dividir(operando1, operando2).toString();
+                        document.getElementById("oculto").value = document.getElementById("display").innerHTML + "/=";
+                        if (resultado.indexOf(".") != -1) {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 9);
+                        }
+                        else {
+                            document.getElementById("display").innerHTML = resultado.toString().substring(0, 8);
+                        }
+                    }
 
                 }
 
@@ -173,17 +241,69 @@ function calculadora(caracter) {
 
 
             } else {
-                operando1 = document.getElementById("display").innerHTML;
-                document.getElementById("display").innerHTML = "0";
-                document.getElementById("oculto").value = operando1 + signo;
+
+                if (operando1.indexOf("=") != -1) {
+                    
+
+                        document.getElementById("oculto").value = document.getElementById("display").innerHTML + signo;
+                        document.getElementById("display").innerHTML = "";
+                        
+
+                } else {
+                    if (operando1.indexOf("+") != -1) {
+                        operando1 = parseFloat(operando1.split("+"));
+                        operando2 = parseFloat(operando2);
+                        resultado = sumar(operando1, operando2).toString();
+                        document.getElementById("display").innerHTML = "";
+                        document.getElementById("oculto").value = resultado + signo;
+
+
+
+
+
+                    } else if (operando1.indexOf("-") != -1) {
+
+                        operando1 = parseFloat(operando1.split("-"));
+                        operando2 = parseFloat(operando2);
+                        resultado = restar(operando1, operando2).toString();
+                        document.getElementById("display").innerHTML = "";
+                        document.getElementById("oculto").value = resultado + signo;
+
+
+
+                    } else if (operando1.indexOf("*") != -1 && document.getElementById("oculto").value != "0") {
+                        operando1 = parseFloat(operando1.split("*"));
+                        operando2 = parseFloat(operando2);
+                        resultado = multiplicar(operando1, operando2).toString();
+                        document.getElementById("display").innerHTML = "";
+                        document.getElementById("oculto").value = resultado + signo;
+
+
+
+
+
+
+                    } else if (operando1.indexOf("/") != -1 && document.getElementById("oculto").value != "0") {
+                        operando1 = parseFloat(operando1.split("/"));
+                        operando2 = parseFloat(operando2);
+                        resultado = dividir(operando1, operando2).toString();
+                        document.getElementById("display").innerHTML = "";
+                        document.getElementById("oculto").value = resultado + signo;
+
+
+
+
+                    }
+                    else {
+                        document.getElementById("oculto").value = document.getElementById("display").innerHTML + signo;
+                        document.getElementById("display").innerHTML = "";
+                        
+                    }
+                }
 
 
 
             }
-
-
-
-
         }
 
 
