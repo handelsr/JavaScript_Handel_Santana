dd9cac9e0dcd1856b1efeaa9aa651161b9b79374
handelsr <31743655+handelsr@users.noreply.github.com>
2018-10-08 22:47:50 -0400

    Edicion 7

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
+var oculto = "0";
+var elemento = document.getElementsByTagName('img');
+for (var i = "0"; i < elemento.length; i++) {
+
+    var NoPermitidosCalcular = "on, sign, raiz, punto";
+    if (NoPermitidosCalcular.indexOf(elemento[i].id) == -1) {
+        var caracter;
+        if (elemento[i].id == "dividido") {
+            caracter = "/";
+        }
+        else if (elemento[i].id == "por") {
+            caracter = "*";
+        }
+        else if (elemento[i].id == "menos") {
+            caracter = "-";
+        }
+        else if (elemento[i].id == "igual") {
+            caracter = "=";
+        }
+        else if (elemento[i].id == "mas") {
+            caracter = "+";
+        }
+        else {
+            caracter = elemento[i].id;
+        }
+        elemento[i].addEventListener("mousedown", calculadora.bind(this, caracter));
+    } else if (elemento[i].id=="on") {
+        elemento[i].addEventListener("mousedown", limpiar_todo);
+    } else if (elemento[i].id == "sign") {
+        elemento[i].addEventListener("mousedown", agregar_signo);
+    } else if (elemento[i].id == "punto") {
+        elemento[i].addEventListener("mousedown", agregar_punto);
+    }
+
+    elemento[i].addEventListener("mousedown", reducir_width.bind(this, elemento[i].id));
+    elemento[i].addEventListener("mouseup", aumentar_width);
+}
+
+limpiar_todo();
+
 function sumar(operando1, operando2) {
     return operando1 + operando2;
 }
@@ -17,13 +57,17 @@ function dividir(operando1, operando2) {
 
 function limpiar_todo() {
     document.getElementById("display").innerHTML = "0";
-    document.getElementById("oculto").value = "0";
+    oculto = "0";
 }
 
 
-function aumentar_width(id) {
+function aumentar_width() {
 
-    document.getElementById(id).style.width = "80px";
+    var imagenes = document.getElementsByTagName('img');
+
+    for (var i = 0; i < imagenes.length; i++) {
+        imagenes[i].style.width = "80px";
+    }
 }
 
 function reducir_width(id) {
@@ -37,11 +81,15 @@ function reducir_width(id) {
 function agregar_punto() {
     var cantidad_actual = document.getElementById("display").innerHTML;
 
-
+    if (cantidad_actual.length < 8) {
     if (cantidad_actual.indexOf(".") == -1) {
         cantidad_actual = cantidad_actual + ".";
 
         document.getElementById("display").innerHTML = cantidad_actual;
+    }
+    } else {
+        alert("lo sentimos, solo se permiten hasta 8 caracteres");
+        aumentar_width();
     }
 
 
@@ -71,21 +119,19 @@ function agregar_signo() {
 }
 
 
+
+
 function calculadora(caracter) {
     //funcion para tomar los caracters dados por el usuario y mostrarlo en pantalla
     var anterior = document.getElementById("display").innerHTML; // controlar lo que se mostrara en pantalla
     var anteriornum = document.getElementById("display").innerHTML.toString();
     var anteriornum = anteriornum.toString().replace(".", "");
-    var anteriornum = anteriornum.toString().replace("/", "");
-    var anteriornum = anteriornum.toString().replace("=", "");
-    var anteriornum = anteriornum.toString().replace("+", "");
-    var anteriornum = anteriornum.toString().replace("-", "");
-    var anteriornum = anteriornum.toString().replace("*", "");
     var numeros = "0123456789";
+    var signos = "/*-+=";
     var resultado = "0";
 
 
-    if (anteriornum.length < 8) {
+    if (anteriornum.length < 8 || signos.indexOf(caracter) != -1) {
 
         if (numeros.indexOf(caracter) != -1) { // si el caracter ingresado se encuentra entre los numeros validos, si no  es un signo de operacion
 
@@ -125,7 +171,7 @@ function calculadora(caracter) {
             var operando2;
 
             operando2 = document.getElementById("display").innerHTML;
-            operando1 = document.getElementById("oculto").value;
+            operando1 = oculto;
 
             if (signo == "=") {
 
@@ -147,7 +193,7 @@ function calculadora(caracter) {
                         operando1 = parseFloat(operando1.split("+"));
                         operando2 = parseFloat(operando2);
                         resultado = sumar(operando1, operando2).toString();
-                        document.getElementById("oculto").value = document.getElementById("display").innerHTML + "+=";
+                        oculto = document.getElementById("display").innerHTML + "+=";
                         if (resultado.indexOf(".") != -1) {
                             document.getElementById("display").innerHTML = resultado.toString().substring(0, 9);
                         }
@@ -174,7 +220,7 @@ function calculadora(caracter) {
                         operando1 = parseFloat(operando1.split("-"));
                         operando2 = parseFloat(operando2);
                         resultado = restar(operando1, operando2).toString();
-                        document.getElementById("oculto").value = document.getElementById("display").innerHTML + "-=";
+                        oculto = document.getElementById("display").innerHTML + "-=";
                         if (resultado.indexOf(".") != -1) {
                             document.getElementById("display").innerHTML = resultado.toString().substring(0, 9);
                         }
@@ -183,7 +229,7 @@ function calculadora(caracter) {
                         }
                     }
 
-                } else if (operando1.indexOf("*") != -1 && document.getElementById("oculto").value != "0") {
+                } else if (operando1.indexOf("*") != -1 && oculto != "0") {
 
                     if (operando1.indexOf("=") != -1) {
                         operando1 = parseFloat(operando1.split("*"));
@@ -199,7 +245,7 @@ function calculadora(caracter) {
                         operando1 = parseFloat(operando1.split("*"));
                         operando2 = parseFloat(operando2);
                         resultado = multiplicar(operando1, operando2).toString();
-                        document.getElementById("oculto").value = document.getElementById("display").innerHTML + "*=";
+                        oculto = document.getElementById("display").innerHTML + "*=";
                         if (resultado.indexOf(".") != -1) {
                             document.getElementById("display").innerHTML = resultado.toString().substring(0, 9);
                         }
@@ -210,7 +256,7 @@ function calculadora(caracter) {
 
 
 
-                } else if (operando1.indexOf("/") != -1 && document.getElementById("oculto").value != "0") {
+                } else if (operando1.indexOf("/") != -1 && oculto != "0") {
 
                     if (operando1.indexOf("=") != -1) {
                         operando1 = parseFloat(operando1.split("/"));
@@ -226,7 +272,7 @@ function calculadora(caracter) {
                         operando1 = parseFloat(operando1.split("/"));
                         operando2 = parseFloat(operando2);
                         resultado = dividir(operando1, operando2).toString();
-                        document.getElementById("oculto").value = document.getElementById("display").innerHTML + "/=";
+                        oculto = document.getElementById("display").innerHTML + "/=";
                         if (resultado.indexOf(".") != -1) {
                             document.getElementById("display").innerHTML = resultado.toString().substring(0, 9);
                         }
@@ -245,7 +291,7 @@ function calculadora(caracter) {
                 if (operando1.indexOf("=") != -1) {
                     
 
-                        document.getElementById("oculto").value = document.getElementById("display").innerHTML + signo;
+                        oculto = document.getElementById("display").innerHTML + signo;
                         document.getElementById("display").innerHTML = "";
                         
 
@@ -255,7 +301,7 @@ function calculadora(caracter) {
                         operando2 = parseFloat(operando2);
                         resultado = sumar(operando1, operando2).toString();
                         document.getElementById("display").innerHTML = "";
-                        document.getElementById("oculto").value = resultado + signo;
+                        oculto = resultado + signo;
 
 
 
@@ -267,35 +313,35 @@ function calculadora(caracter) {
                         operando2 = parseFloat(operando2);
                         resultado = restar(operando1, operando2).toString();
                         document.getElementById("display").innerHTML = "";
-                        document.getElementById("oculto").value = resultado + signo;
+                        oculto = resultado + signo;
 
 
 
-                    } else if (operando1.indexOf("*") != -1 && document.getElementById("oculto").value != "0") {
+                    } else if (operando1.indexOf("*") != -1 && oculto != "0") {
                         operando1 = parseFloat(operando1.split("*"));
                         operando2 = parseFloat(operando2);
                         resultado = multiplicar(operando1, operando2).toString();
                         document.getElementById("display").innerHTML = "";
-                        document.getElementById("oculto").value = resultado + signo;
+                        oculto = resultado + signo;
 
 
 
 
 
 
-                    } else if (operando1.indexOf("/") != -1 && document.getElementById("oculto").value != "0") {
+                    } else if (operando1.indexOf("/") != -1 && oculto != "0") {
                         operando1 = parseFloat(operando1.split("/"));
                         operando2 = parseFloat(operando2);
                         resultado = dividir(operando1, operando2).toString();
                         document.getElementById("display").innerHTML = "";
-                        document.getElementById("oculto").value = resultado + signo;
+                        oculto = resultado + signo;
 
 
 
 
                     }
                     else {
-                        document.getElementById("oculto").value = document.getElementById("display").innerHTML + signo;
+                        oculto = document.getElementById("display").innerHTML + signo;
                         document.getElementById("display").innerHTML = "";
                         
                     }
@@ -309,6 +355,7 @@ function calculadora(caracter) {
 
     } else {
         alert("lo sentimos, solo se permiten hasta 8 caracteres");
+        aumentar_width();
     }
 
 
